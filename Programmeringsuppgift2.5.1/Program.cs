b3f981d4111f71ef08b849abe85b081b3dbe9503
DommSite <johan.johansson06@live.se>
2023-09-20 10:43:17 +0200

    Add project files.

---
+++ Programmeringsuppgift2.5.1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+using System.Diagnostics.Metrics;
+
+namespace övning2_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv en multiplikation (x*y)");
+            string skrivenmultiplikation = Console.ReadLine();
+            int multi = skrivenmultiplikation.IndexOf("*");
+            string tal1text = skrivenmultiplikation[..multi];
+            string tal2text = skrivenmultiplikation[(multi + 1)..];
+
+            float tal1 = float.Parse(tal1text);
+            float tal2 = float.Parse(tal2text);
+            float summa = tal1 * tal2;
+
+            Console.WriteLine(skrivenmultiplikation + "=" + summa);
+
+            Console.ReadKey();
+        }
+    }
+}
